cacccff050e81352b9275a76578e72159e946b9f
Delwin Campbell <d@delw.in>
2013-07-10 17:04:20 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require("fs");
 
 var app = express.createServer(express.logger());
 
+var contents = fs.readFileSync("index.html");
+var buffer = new Buffer(contents, "utf8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
